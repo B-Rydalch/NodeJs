f22b93f00a1136766705182665f528e8eeabd9fc
Brad Rydalch <bradrydalch@gmail.com>
2018-11-25 16:52:36 -0700

    added backend extensions to begin working with db

---
+++ public/db/setup.sql
@@ -0,0 +1,34 @@
+DROP TABLE parents;
+DROP TABLE children; 
+DROP TABLE grounded; 
+
+CREATE TABLE parents
+(
+    id SERIAL PRIMARY KEY
+    , name varchar(50) NOT NULL
+    , password VARCHAR(255)  NOT NULL
+);
+
+CREATE TABLE children
+(
+    id SERIAL PRIMARY KEY -- primary key column
+    , name VARCHAR(50) NOT NULL
+    , age  SMALLINT  NOT NULL
+);
+
+CREATE TABLE grounded
+(
+    id SERIAL PRIMARY KEY -- primary key column
+    , item_name VARCHAR(50)  NOT NULL
+    , number_times_grounded INT NULL
+    , duration TIMESTAMP NULL 
+    , children_id SMALLINT   NOT NULL REFERENCES children(id)
+    -- specify more columns here
+);
+
+/*************DEFAULT DATA ****************/
+INSERT INTO parents (name,password) values('DAD','password'), ('mom', 'mom');
+
+INSERT INTO children (name, age) VALUES('ELI', 5)
+                                    ,  ('LIAM', 4)
+                                    ,  ('CARTER', 2);
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 // const postOffice = require('../CS313-Node/public/js/postal.js')
-
 let urlencodedParser = bodyParser.urlencoded({extended:false});
+const dbConnectionString = "heroku pg:psql postgresql-pointy-85036 --app immense-everglades-17729";
 
 express()
   .use(express.static(__dirname + '/public'))
@@ -16,4 +16,36 @@ express()
         //  (req, res) =>console.log(req.body),
         (req, res) => res.render('pages/postal.ejs', {data:req.body}))
       //   (req, res) => postOffice.postalServices(res,req))
+  .get("/child", getChild) 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  // (get) child info
+  function getChild(req, res) {
+    console.log ("getting child info");
+    res.json({name: "Eli"});
+  }
+
+  // (get) list of children
+  function listChild(req, res) {
+    console.log ("getting child info");
+    res.json({name: "Eli"});
+  }
+
+  // (post) insert grounding 
+  function grounded(req, res) {
+    console.log ("insert grounding");
+    res.json({name: "Eli"});
+  }
+
+  // (post) delete grounding
+  function deleteGrounding(req, res) {
+    console.log ("delete grounding");
+    res.json({name: "Eli"});
+  }
+
+  // (put) update grounding => increasing time, adding items taken away
+  function updateGrounding(req, res) {
+    console.log ("update grounding");
+    res.json({name: "Eli"});
+  }
