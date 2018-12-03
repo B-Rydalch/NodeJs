7b6c7f022b4cc16a0ff7ae5c5ebf1931985e3f75
Brad Rydalch <bradrydalch@gmail.com>
2018-12-03 15:21:55 -0700

    front-end ready for google api and data

---
+++ public/js/grounding.js
@@ -0,0 +1,27 @@
+// Google API 
+google.charts.load("current", {packages: ["line"]});
+// set a callback to run when the Google Visualization API is loaded.-->
+google.charts.setOnLoadCallback(drawChart);
+
+
+
+var materialOptions = {
+    chart: {
+      title: 'Average Temperatures and Daylight in Iceland Throughout the Year'
+    },
+    width: 900,
+    height: 500,
+    series: {
+      // Gives each series an axis name that matches the Y-axis below.
+      0: {axis: 'Temps'},
+      1: {axis: 'Daylight'}
+    },
+    axes: {
+      // Adds labels to each axis; they don't have to match the axis names.
+      y: {
+        Temps: {label: 'Temps (Celsius)'},
+        Daylight: {label: 'Daylight'}
+      }
+    }
+  };
+  
