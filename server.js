9ea623089e546fbfdf9af461215a9876b42dd8bd
Brad Rydalch <bradrydalch@gmail.com>
2018-12-04 16:33:16 -0700

    bug searching

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 // const postOffice = require('../CS313-Node/public/js/postal.js')
-const familyServices = require("./public/js/grounding");
+// const familyServices = require("./public/js/grounding");
 let urlencodedParser = bodyParser.urlencoded({extended:true});
 // const { Pool } = require('pg');
 const dbConnectionString = process.env.DATABASE_URL || "heroku pg:psql postgresql-pointy-85036 --app immense-everglades-17729";
@@ -22,12 +22,12 @@ express()
         //  (req, res) =>console.log(req.body),
         (req, res) => res.render('pages/postal.ejs', {data:req.body}))
       //   (req, res) => postOffice.postalServices(res,req))
-  .get("/grounding",urlencodedParser,familyServices)
-  .get("/child", getChild) 
-  .get("/family",listFamily)
-  .get("/listChildren", listChildren)
-  .post("/addChild", addChild)
-  .post("/grounded", grounded)
+  // .get("/grounding",urlencodedParser,familyServices)
+  // .get("/child", getChild) 
+  // .get("/family",listFamily)
+  // .get("/listChildren", listChildren)
+  // .post("/addChild", addChild)
+  // .post("/grounded", grounded)
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
