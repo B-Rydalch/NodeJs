cd6837f685abf21d8e4262bc4b30548cf5a4dcf8
Brad Rydalch <bradrydalch@gmail.com>
2018-12-04 10:00:41 -0700

    changing to ejs

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ express()
         //  (req, res) =>console.log(req.body),
         (req, res) => res.render('pages/postal.ejs', {data:req.body}))
       //   (req, res) => postOffice.postalServices(res,req))
+  .get()
   .get("/child", getChild) 
   .get("/listChildren", listChildren)
   .post("/addChild", addChild)
